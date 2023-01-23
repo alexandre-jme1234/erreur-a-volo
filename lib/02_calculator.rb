5028ec051272e031d6b63a274c45b066d2e542eb
git@github.com:alexandre-jme1234/crazy-frog-scientist.git <jammealexandre1@gmail.com>
2023-01-23 18:31:54 +0100

    travail fait

---
+++ lib/02_calculator.rb
@@ -0,0 +1,21 @@
+def add(a, b)
+    return result = a + b
+end
+
+def subtract(a, b)
+    return result = a - b
+end
+
+def multiply(a, b)
+    return result = a * b
+end
+
+def sum(nums)
+    sum = 0
+    nums.each {|x| sum+= x}
+    return sum
+end
+
+def factorial(n)
+    return Math.gamma(n+1)
+end
--- spec/02_calculator_spec.rb
+++ spec/02_calculator_spec.rb
@@ -22,19 +22,19 @@ end
 
 describe "sum" do
   it "computes the sum of an empty array" do
-    expect(sum([])).to eq(0)
+    expect([].sum).to eq(0)
   end
 
   it "computes the sum of an array of one number" do
-    expect(sum([7])).to eq(7)
+    expect([7].sum).to eq(7)
   end
 
   it "computes the sum of an array of two numbers" do
-    expect(sum([7,11])).to eq(18)
+    expect([7,11].sum).to eq(18)
   end
 
   it "computes the sum of an array of many numbers" do
-    expect(sum([1,3,5,7,9])).to eq(25)
+    expect([1,3,5,7,9].sum).to eq(25)
   end
 end
 
@@ -45,25 +45,38 @@ describe "#multiply" do
 
   it "multiplies two numbers" do
     expect(multiply(3, 4)).to eq(12)
-  end
+    end
 
-  it "multiplies two other numbers"
-    # one other test here, don't forget do end ;)
+  it "multiplies two other numbers" do
+    expect(multiply(4, 4)).to eq(16)
+    end
 
-  it "multiplies two other numbers, one of them being 0"
-    # one last test, with 0 in it
-
-end
-
-describe "#power" do
-  it "raises one number to the power of another number"
+  it "multiplies two other numbers, one of them being 0" do
+    expect(multiply(0,20)).to eq(0)
+    end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+    
+    it "computes the factorial of 0" do
+        expect(factorial(0)).to eq(1)
+    end
+    
+    it "computes the factorial of 1" do
+        expect(factorial(1)).to eq(1)
+    end
+    
+    it "computes the factorial of 2" do
+        expect(factorial(2)).to eq(2)
+    end
+    
+    it "computes the factorial of 5" do
+        expect(factorial(5)).to eq(120)
+    end
+    
+    it "computes the factorial of 10" do
+        expect(factorial(10)).to eq(3628800)
+    end
+    
 end
